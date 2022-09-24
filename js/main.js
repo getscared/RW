c6729abe28b8d8317d9107b49db469215934b6ab
Ostroushko_Anton <ostroushko.anton91@gmail.com>
2022-09-24 21:16:34 +0300

    Navigatiun by url

--- js/main.js
+++ js/main.js
@@ -1,19 +1,55 @@
+// Бекграунд для header
 function activeHeader() {
   const header = document.querySelector(".header");
-  window.onscroll = function () {
+  window.addEventListener("scroll", function () {
     if (window.pageYOffset > 50) {
       header.classList.add("header__active");
     } else {
       header.classList.remove("header__active");
     }
-  };
+  });
 }
 
 activeHeader();
 
+//Навигация по ссылкам. Без изменения URL
+
 const orderBtn = document.getElementById("order_btn");
-orderBtn.addEventListener("click", scrollIntoElement());
-function ScrollToElement() {
-  const idCompany = document.getElementById("company");
-  window.scrollIntoView(idCompany);
+const solutionsBtn = document.getElementById("solutions_btn");
+const featuresBtn = document.getElementById("features_btn");
+const pricingBtn = document.getElementById("pricing_btn");
+const companygBtn = document.getElementById("company_btn");
+
+const idOrderContent = document.getElementById("order_content");
+const idSolutions = document.getElementById("solutions");
+const idFeatures = document.getElementById("features");
+const idPricing = document.getElementById("pricing");
+const idCompany = document.getElementById("company");
+
+function ScrollToElement(element) {
+  element.scrollIntoView({ behavior: "smooth" });
 }
+
+orderBtn.addEventListener("click", function () {
+  ScrollToElement(idOrderContent);
+});
+solutionsBtn.addEventListener("click", function () {
+  ScrollToElement(idSolutions);
+});
+featuresBtn.addEventListener("click", function () {
+  ScrollToElement(idFeatures);
+});
+pricingBtn.addEventListener("click", function () {
+  ScrollToElement(idPricing);
+});
+companygBtn.addEventListener("click", function () {
+  ScrollToElement(idCompany);
+});
+
+//Переключение изображений.
+
+const creatingOption = document.querySelector(".creating__option");
+
+creatingOption.addEventListener("click", () => {
+  creatingOption.classList.add("creating__option__active");
+});
